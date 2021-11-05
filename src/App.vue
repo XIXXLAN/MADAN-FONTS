705961c6f487fbe3da5b0ee8959e79af5f75cb99
XIXXLAN <111637525+XIXXLAN@users.noreply.github.com>
2021-11-05 18:18:20 +0000

    mm/hugetlb.c: add cond_resched_lock() in return_unused_surplus_pages()

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
   <main>
     <px-header v-bind:links="links" />
-    <router-view class="c
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+  </main>
+</template>
+
+<script>
+import PxHeader from "@/components/PxHeader";
