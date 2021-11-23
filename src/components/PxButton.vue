99d35edce0cddd606cd87eac2e048f07ee3ee495
XIXXLAN <111637525+XIXXLAN@users.noreply.github.com>
2021-11-23 20:25:41 +0000

    Reviewed by Oliver Hunt.

--- src/components/PxButton.vue
+++ src/components/PxButton.vue
@@ -1,4 +1,7 @@
 <template>
   <button
     v-on:click="buttonClick"
-    class="bg-transparent hover:bg-green-500 tex
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
+  >
+    <beat-loader
+      v-b
