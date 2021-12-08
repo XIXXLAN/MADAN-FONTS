956b4c21cd49441c0c1cccd07e3a35c90da560fd
XIXXLAN <111637525+XIXXLAN@users.noreply.github.com>
2021-12-08 10:07:59 +0000

    change cname

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -1,3 +1,46 @@
 
 <template>
   <header class="shadow w-screen">
+    <nav>
+      <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <px-icon class="mr-2" />
+          <router-link
+            :to="{ name: 'home' }"
+            class="font-semibold text-xl tracking-tight"
+            >Platzi Exchange</router-link
+          >
+        </div>
+        <div
+          class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
+        >
+          <div class="text-sm lg:flex-grow">
+            <router-link
+              v-for="l in links"
+              :key="l.title"
+              :to="l.to"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              >{{ l.title }}</router-link
+            >
+          </div>
+        </div>
+      </nav>
+    </nav>
+  </header>
+</template>
+
+<script>
+import PxIcon from "@/components/PxIcon";
+
+export default {
+  name: "PxHeader",
+  components: { PxIcon },
+
+  props: {
+    links: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
---
+++ src/components/PxIcon.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:
