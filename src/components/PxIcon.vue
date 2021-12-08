20c7f06c6f5071c2af05e75964a30a9ced4c29f8
XIXXLAN <111637525+XIXXLAN@users.noreply.github.com>
2021-12-08 11:29:44 +0000

    updated keys

--- src/components/PxIcon.vue
+++ src/components/PxIcon.vue
@@ -3,4 +3,9 @@
     version="1.1"
     id="Layer_1"
     xmlns="http://www.w3.org/2000/svg"
-    xmlns:
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    width="40px"
+    viewBox="0 0 937 853"
+    enable-backgro
