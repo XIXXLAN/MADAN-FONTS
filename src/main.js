62f799bccf5d4461ffa718015d68a05868cfe02c
XIXXLAN <111637525+XIXXLAN@users.noreply.github.com>
2022-12-31 16:49:04 +0000

    readme.md update

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 import { createApp } from "vue";
-impor
+import App from "./App.vue";
+import "@/assets/css/tailwind.css";
+
+import VueChartkick from "vue-chartkick";
+import "chartkick/chart.js";
+
+import rout
