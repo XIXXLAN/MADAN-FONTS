b22f038b49786418d9f4a000505e013aeda88a3d
XIXXLAN <111637525+XIXXLAN@users.noreply.github.com>
2023-01-01 18:02:24 +0000

    fs/dyn_fsync: check dyn fsync control's active prior to performing fsync ops

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,4 +3,8 @@
     <h1 class="text-gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
     <p class="text-gray-600 text-xl text-center">
       Proyecto para obtener las cotizaciones de las cryptomonedas mas
-      importantes a traves de la API RE
+      importantes a traves de la API REST de Coincap.
+      <br />Este proyecto es utilizado en el curo de Vue.js Basico de Platzi
+    </p>
+
+    <router-link to="/" class
