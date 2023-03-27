2953e95c1996953d19523e9e16fb6282e03c1d36
XIXXLAN <111637525+XIXXLAN@users.noreply.github.com>
2023-03-27 15:17:29 +0000

    updates deps

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,8 @@
 <template>
   <div>
     <bounce-loader
-      v-bind:loading="isLoading
+      v-bind:loading="isLoading"
+      v-bind:color="'#68d391'"
+      v-bind:size="'100px'"
+    ></bounce-loader>
+    <px-assets-table v-if="!isLoading" v-bind:assets="a
