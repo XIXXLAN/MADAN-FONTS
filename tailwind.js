01752e08618fb547abcf6d5562f70e00bee899c0
XIXXLAN <111637525+XIXXLAN@users.noreply.github.com>
2023-04-03 11:16:51 +0000

    Adding evaluation data.

--- tailwind.js
+++ tailwind.js
@@ -9,4 +9,13 @@ module.exports = {
       body: ["Graphik", "sans-serif"],
     },
     extend: {
-    
+      colors: {
+        cyan: "#9cdbff",
+      },
+      margin: {
+        96: "24rem",
+        128: "32rem",
+      },
+    },
+  },
+  vari
